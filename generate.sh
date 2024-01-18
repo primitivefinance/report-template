d024a5ee8d2df9f1e10ba90bfce47f2b763c2a00
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-01-18 14:33:10 -0500

    dispute game resources

--- generate.sh
+++ generate.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# pandoc with gfm flavored markdown seems to have issues regarding
-# A temporary work around to have page breaks.
-# FIXME figure out a way to natively do this.
-sed -i 's/textbackslash clearpage/clearpage/g' report.tex
-# On github CI, pandoc seems to be generating the following
-sed -i 's/textbackslash{}clearpage/clearpage/g' report.tex
-xelatex -shell-escape -interaction nonstopmode main.tex
-# Running it a second time to generate references
 xelatex -shell-escape -interaction nonstopmode main.tex
